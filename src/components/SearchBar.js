c5dd036ec2cfc3857df52c8a66f6d713d73422b2
Andrew Tran <andrewtttran@gmail.com>
2020-07-29 21:46:43 +1000

    search bar and bit of search controller

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/SearchBar.css';
 //search bar for finding recipes
 
+// takes in props from SearchController
 const SearchBar = (props) => {
   return (
     <div className='searchbar'>
@@ -10,10 +11,13 @@ const SearchBar = (props) => {
         data-cy='searchbar'
         placeholder='Type your search term here and hit enter'
         type='text'
+        // set the value to be that of the searchValue
         value={props.searchValue}
+        // any time the input changes call props.onSearchValueChange on the event.target.value which should be the input
         onChange={(event) => {
           props.onSearchValueChange(event.target.value);
         }}
+        //when the enter key is pressed, call props.onEnter() which should get recipes for us in SearchController.js
         onKeyUp={(event) => {
           if (event.keyCode === 13) {
             props.onEnter();
