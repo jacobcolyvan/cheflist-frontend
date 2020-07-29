a0ffba143949a27cc2445157fa8a9a2df58fd1ed
Andrew Tran <andrewtttran@gmail.com>
2020-07-29 22:41:42 +1000

    comments, comments everywhere

--- src/components/Backdrop.js
+++ src/components/Backdrop.js
@@ -5,7 +5,7 @@ import '../styles/Backdrop.css';
 
 const Backdrop = (props) => {
   // using a portal to render a backdrop
-  //needed for the modal
+  //needed for the modal so that we can render it on a different dom element
   return ReactDOM.createPortal(
     <div className='backdrop' onClick={props.onClick}></div>,
     document.getElementById('backdrop-hook')
