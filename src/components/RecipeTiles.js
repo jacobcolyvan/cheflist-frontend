b0576fc953570f0d1c891b0f663d1e2f8a1693e5
Jacob Colyvan <jacob_colyvan@live.com>
2020-07-30 19:36:12 +1000

    tidied up detailed-recipe-view; minor styling changes

--- src/components/RecipeTiles.js
+++ src/components/RecipeTiles.js
@@ -4,6 +4,7 @@ import '../styles/RecipeTile.css';
 //renders list of searched recipes
 const RecipeTiles = ({ recipes, saveRecipe }) => {
   // takes in recipes (object) and saveRecipe = callback function to save a recipe
+  console.log(recipes.diets);
   return (
     <div className='recipe-results-container'>
       {/* map each recipe to a div with recipe${index} as the key, each div will contain a header, image, list od diets and button to favourite/save*/}
