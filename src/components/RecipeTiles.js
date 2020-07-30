088900bd128af78340927a793e796ec3191f50a1
Jacob Colyvan <jacob_colyvan@live.com>
2020-07-30 19:51:14 +1000

    minor styling changes

--- src/components/RecipeTiles.js
+++ src/components/RecipeTiles.js
@@ -4,7 +4,6 @@ import '../styles/RecipeTile.css';
 //renders list of searched recipes
 const RecipeTiles = ({ recipes, saveRecipe }) => {
   // takes in recipes (object) and saveRecipe = callback function to save a recipe
-  console.log(recipes.diets);
   return (
     <div className='recipe-results-container'>
       {/* map each recipe to a div with recipe${index} as the key, each div will contain a header, image, list od diets and button to favourite/save*/}
@@ -13,19 +12,30 @@ const RecipeTiles = ({ recipes, saveRecipe }) => {
           <h3>{recipe.title}</h3>
 
           <img className='image' src={recipe.image} alt='' />
+          {recipe.diets.length > 0 && (
+            <ul className='diets'>
+              {/* map each diet with to a list */}
+              {recipe.diets.map((diet, index) => (
+                <li key={`diet${index}`}>{diet}</li>
+              ))}
+            </ul>
+          )}
 
-          <ul>
-            {/* map each diet with to a list */}
-            {recipe.diets.map((diet, index) => (
-              <li key={`diet${index}`}>{diet}</li>
-            ))}
-          </ul>
+          {recipe.cuisines.length > 0 && (
+            <p>
+              {recipe.cuisines.map((cuisines, index) => (
+                <span key={`cuisines${index}`}>{cuisines} </span>
+              ))}
+            </p>
+          )}
+
           <button
             // when button is clicked callback function saveRecipe is called on the specified index
             data-cy='save'
             onClick={() => {
               saveRecipe(index);
             }}
+            className='save-button'
           >
             Favourite/Save
           </button>
